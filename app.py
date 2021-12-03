4792551620da6cf63536b749f2caf88d4dc24662
Nikita Dukin <nikirov14@mail.ru>
2021-12-03 18:25:29 +0300

    Delete code check and changed json type

--- app.py
+++ app.py
@@ -6,21 +6,35 @@ import pymorphy2
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
-
-def get_best_word(options, words, amount):
+def get_best_word(options, core_object, amount):
     metrics = {}
-    for word in words.keys():
-        if options not in words[word]["tags"]:
+    dict_to_return_words = {}
+
+    for tag in core_object.get("tags").keys():
+        if tag != options:
             continue
-        value = int(words[word]["failed_calls"]) / (int(words[word]["failed_calls"]) + int(words[word]["success_calls"]))\
-                * 1 / (int(words[word]["success_calls"] + 1))
-        metrics[word] = value
+        for word in core_object.get("tags").get(tag):
+            core_object.get("words").get(word)
+            value = int(core_object.get("words").get(word)["failed_calls"]) / (
+                        int(core_object.get("words").get(word)["failed_calls"]) + int(
+                    core_object.get("words").get(word)["success_calls"])) \
+                    * 1 / (int(core_object.get("words").get(word)["success_calls"] + 1))
+            metrics[(word, core_object["words"][word]["translation"])] = value
+
+    # for word in core_object.get("words").keys():
+    #    if options not in core_object.get("tags"):
+    #        continue
+    #    value = int(core_object["words"][word]["failed_calls"]) / (int(core_object["words"][word]["failed_calls"]) + int(core_object["words"][word]["success_calls"]))\
+    #            * 1 / (int(core_object["words"][word]["success_calls"] + 1))
+    #    metrics[(word, core_object["words"][word]["translation"])] = value
     print(metrics)
     to_return = sorted(metrics.items(), key=lambda x: - x[1])[:min(amount, len(metrics))]
-    return [cort[0] for cort in to_return]
+    to_return = [cort[0] for cort in to_return]
+
+    for el in to_return:
+        dict_to_return_words[el[0]] = el[1]
+
+    return {"words": dict_to_return_words}
 
 
 @app.route('/getwords')
@@ -28,25 +42,14 @@ def get_current_words_by_code():
     code = request.args.get('code')
     option = request.args.get('option')
     amount = request.args.get('amount')
-    # Код - 16 цифр. Первые 8 - id пользователя. Вторые 8 - режим.
-    # Если  вектор id скалярно умножить на режим, а потом взять остаток от деления на 71
-    # 0 - работа со словами/статистика
-    # 1 - добавление слов
-    # 2 - добавление слов и работа со словами/статистика
 
-    # 1*2+2*9+0*2+5*5+5*2+0*0+2*3+5*2 % 71 = 0
-    # 12055025 - id, 29252032 - код доступа на работу со словами/статистикой
     try:
-        id = str(code[:8])
-        private = str(code[8:])
-        if sum([int(id[i]) * int(private[i]) for i in range(len(id))]) % 71 not in [0, 2]:
-            return "Error. No roots"
+        id = str(code)
     except:
         return "Error. No roots"
 
-    with open("data.json", "r") as read_file:
+    with open("data1.json", "r") as read_file:
         data = json.load(read_file)
-
     try:
         words = get_best_word(option, data.get(id), int(amount))
         return str(words)
